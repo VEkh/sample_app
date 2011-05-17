45f7ae7ccb18d9384fbb43fcf1d2cfab97324a27
Vidal Ekechukwu <vekechuk@aol.com>
2011-05-17 10:58:22 -0400

    Added a stub User showpage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110512200719) do
+ActiveRecord::Schema.define(:version => 20110511152326) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,7 +18,5 @@ ActiveRecord::Schema.define(:version => 20110512200719) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
