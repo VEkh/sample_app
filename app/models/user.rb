1cf2415c26aab9426583fd4efb460c1ce9ffdfe3
Vidal Ekechukwu <vekechuk@aol.com>
2011-05-21 11:03:05 -0400

    Working user signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110517233659
+# Schema version: 20110518110300
 #
 # Table name: users
 #
@@ -9,6 +9,7 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -36,8 +37,12 @@ class User < ActiveRecord::Base
 
 		def authenticate(email, submitted_password)
 			user = find_by_email(email)
-			return nil 	if user.nil?
-			return user	if user.has_password?(submitted_password)
+			(user && user.has_password?(submitted_password)) ? user : nil
+		end
+		
+		def authenticate_with_salt(id, cookie_salt)
+			user = find_by_id(id)
+			(user && user.salt == cookie_salt) ? user : nil
 		end
 	end
 
