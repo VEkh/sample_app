90f836602ff5b28cd2b14c8e5bff5ff4da03f7f0
Vidal Ekechukwu <vekechuk@aol.com>
2011-05-24 21:54:15 -0400

    Workin index page

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,5 @@
 Factory.define :user do |user|
-	user.name										"Vidal Ekechukwu"
+	user.name										"Vidal Ekechukwu"											
 	user.email									"vekechuk@example.com"
 	user.password								"foobar"
 	user.password_confirmation 	"foobar"
